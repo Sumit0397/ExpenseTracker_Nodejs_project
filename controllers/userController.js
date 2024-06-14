aa54f6235c7c5d851fb3480292f18bcff222b52f
sumit majumdar <sumitmajumdar16@gmail.com>
2024-06-14 17:59:37 +0530

    Task 3: Secure the user's password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const getAuthenticationPage = (req, res, next) => {
     res.sendFile(path.join(__dirname, "../", "public", "views", "signuplogin.html"));
@@ -16,34 +17,41 @@ const postUserSignup = async (req, res, next) => {
         if (user) {
             return res.status(409).json({ message: 'User already exists.' });
         } else {
-            await User.create({ name, email, password });
+            bcrypt.hash(password, 10, async (err, hash) => {
+                await User.create({ name, email, password: hash });
+            })
             res.status(200).json({ message: 'User registered successfully!' });
         }
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({message : 'Internal Server Error!!'});
+        res.status(500).json({ message: 'Internal Server Error!!' });
     }
 }
 
-const postUserLogin = async (req,res,next) => {
+const postUserLogin = async (req, res, next) => {
     try {
-        const {email , password} = req.body;
-        
-        const user = await User.findOne({where : {email : email}});
+        const { email, password } = req.body;
 
-        if(user){
-            if(user.password == password){
-                res.status(200).json({message : 'user logged in succesfully!' , success : true})
-            }else{
-                res.status(401).json({message : 'Password is incorrect!' , success : false})
-            }
-        }else{
-            res.status(404).json({message : 'User Not found! please login with correct credentials' , success : false})
+        const user = await User.findOne({ where: { email: email } });
+
+        if (user) {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result === true) {
+                    res.status(200).json({ message: 'user logged in succesfully!', success: true })
+                } else {
+                    res.status(401).json({ message: 'Password is incorrect!', success: false })
+                }
+                if(err){
+                    throw new Error(err);
+                }
+            })
+        } else {
+            res.status(404).json({ message: 'User Not found! please login with correct credentials', success: false })
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({message : 'Internal Server Error!!' , success : false});
+        res.status(500).json({ message: 'Internal Server Error!!', success: false });
     }
 }
 
